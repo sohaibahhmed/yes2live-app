13a8037f73932c93426e44288c72a846b63daf1c
Maane <emanhaneef8@gmail.com>
2023-03-13 23:48:13 +0500

    ADD COMMENTS

--- app/src/main/java/com/fyp/yes2live/MainActivity.java
+++ app/src/main/java/com/fyp/yes2live/MainActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+/**
+ * MAIN ACTIVITY
+ */
 public class MainActivity extends AppCompatActivity {
     Button button;
     @Override
