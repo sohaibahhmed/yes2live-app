84d1b19dfd4ae5f43ae2b45225a5b922d77b6a88
showbi <ali@flow-solutions.com>
2023-04-04 22:21:10 +0800

    adding home page and login

--- app/src/main/java/com/fyp/yes2live/auth/login.java
+++ app/src/main/java/com/fyp/yes2live/auth/login.java
@@ -5,24 +5,15 @@ import android.os.Bundle;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.fyp.yes2live.R;
-import com.fyp.yes2live.apiConfig.APIClient;
 import com.fyp.yes2live.apiConfig.APIInterface;
 import com.fyp.yes2live.homepage;
-import com.fyp.yes2live.model.User;
-import com.fyp.yes2live.response.BaseResponse;
 
 import java.util.regex.Pattern;
-
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 
 public class login extends AppCompatActivity {
@@ -38,8 +29,8 @@ public class login extends AppCompatActivity {
                     ".{4,}" +                // at least 4 characters
                     "$");
 
-     EditText email;
-     EditText password;
+//     EditText email;
+//     EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,99 +38,102 @@ public class login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         // Referencing email and password
-        email = (EditText) findViewById(R.id.email);
-        password =(EditText) findViewById(R.id.password);
+//        email = (EditText) findViewById(R.id.email);
+//        password =(EditText) findViewById(R.id.password);
 
-       signup_btn = (Button) findViewById(R.id.signupBtn);
-
-       //button1
-        signup_btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-          public void onClick(View v) {
-               Intent intent = new Intent(login.this, signup.class);
-               startActivity(intent);
-          }
-       });
+//       signup_btn = (Button) findViewById(R.id.signupBtn);
+//
+//       //button1
+//        signup_btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//          public void onClick(View v) {
+//               Intent intent = new Intent(login.this, signup.class);
+//               startActivity(intent);
+//          }
+//       });
 
        login_btn = (Button) findViewById(R.id.loginBtn);
         login_btn.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
-               String emailInput = email.getText().toString().trim();
-               String passwordInput = password.getText().toString().trim();
-               validateEmail();
-               validatePassword();
-               if(!validateEmail()||!validatePassword()){
-                   Toast.makeText(login.this,"enter email password" , Toast.LENGTH_SHORT).show();
-               }
-               else {
-                   /**
-                    login endpoint
-                    **/
-                   apiInterface = APIClient.getClient().create(APIInterface.class);
-                   User user = new User(emailInput,passwordInput);
-                   Call<BaseResponse> call = apiInterface.login(user);
-                   call.enqueue(new Callback<BaseResponse>() {
-                       @Override
-                       public void onResponse(Call<BaseResponse> call, Response<BaseResponse> response) {
-                           BaseResponse loginResponse = response.body();
-                           if (loginResponse.getStatus().equals("SUCCESS")) {
-                               Toast.makeText(login.this, loginResponse.getMessage(), Toast.LENGTH_SHORT).show();
-                               Intent intent1 = new Intent(login.this, homepage.class);
-                               startActivity(intent1);
-                           }else{
-                               Toast.makeText(login.this, loginResponse.getMessage(), Toast.LENGTH_SHORT).show();
-                           }
-
-                       }
-
-                       @Override
-                       public void onFailure(Call<BaseResponse> call, Throwable t) {
-                           Toast.makeText(login.this, "Check your internet connection", Toast.LENGTH_SHORT).show();
-                       }
-
-                   });
-               }
+//               String emailInput = "test@gmail.com";//email.getText().toString().trim();
+//               String passwordInput = "test@123";//password.getText().toString().trim();
+//               validateEmail();
+//               validatePassword();
+//               if(!validateEmail()||!validatePassword()){
+//                   Toast.makeText(login.this,"enter email password" , Toast.LENGTH_SHORT).show();
+//               }
+//               else {
+//                   /**
+//                    login endpoint
+//                    **/
+//                   apiInterface = APIClient.getClient().create(APIInterface.class);
+//                   User user = new User(emailInput,passwordInput);
+//                   Call<BaseResponse> call = apiInterface.login(user);
+//                   call.enqueue(new Callback<BaseResponse>() {
+//                       @Override
+//                       public void onResponse(Call<BaseResponse> call, Response<BaseResponse> response) {
+//                           BaseResponse loginResponse = response.body();
+//                           if (loginResponse.getStatus().equals("SUCCESS")) {
+//                               Toast.makeText(login.this, loginResponse.getMessage(), Toast.LENGTH_SHORT).show();
+//                               Intent intent1 = new Intent(login.this, homepage.class);
+//                               startActivity(intent1);
+//                           }else{
+//                               Toast.makeText(login.this, loginResponse.getMessage(), Toast.LENGTH_SHORT).show();
+//                           }
+//
+//                       }
+//
+//                       @Override
+//                       public void onFailure(Call<BaseResponse> call, Throwable t) {
+//                           Toast.makeText(login.this, "Check your internet connection", Toast.LENGTH_SHORT).show();
+//                       }
+//
+//                   });
+//               }
+               //Toast.makeText(login.this, loginResponse.getMessage(), Toast.LENGTH_SHORT).show();
+               Intent intent1 = new Intent(login.this, homepage.class);
+               startActivity(intent1);
            };
        });
     }
     private boolean validateEmail() {
 
         // Extract input from EditText
-        String emailInput = email.getText().toString().trim();
+        String emailInput = "test";//email.getText().toString().trim();
 
         // if the email input field is empty
         if (emailInput.isEmpty()) {
-            email.setError("Field can not be empty");
+            //email.setError("Field can not be empty");
             return false;
         }
 
         // Matching the input email to a predefined email pattern
         else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()) {
-            email.setError("Please enter a valid email address");
+            //email.setError("Please enter a valid email address");
             return false;
         } else {
-            email.setError(null);
+            //email.setError(null);
             return true;
         }
     }
 
     private boolean validatePassword() {
-        String passwordInput = password.getText().toString().trim();
+        String passwordInput = "tests";//password.getText().toString().trim();
         // if password field is empty
         // it will display error message "Field can not be empty"
         if (passwordInput.isEmpty()) {
-            password.setError("Field can not be empty");
+            //password.setError("Field can not be empty");
             return false;
         }
 
         // if password does not matches to the pattern
         // it will display an error message "Password is too weak"
         else if (!PASSWORD_PATTERN.matcher(passwordInput).matches()) {
-            password.setError("Password is too weak");
+            //password.setError("Password is too weak");
             return false;
         } else {
-            password.setError(null);
+            //password.setError(null);
             return true;
         }
     }
@@ -151,9 +145,9 @@ public class login extends AppCompatActivity {
 
         // if the email and password matches, a toast message
         // with email and password is displayed
-        String input = "Email: " + email.getText().toString();
+        String input = "Email: ";// + email.getText().toString();
         input += "\n";
-        input += "Password: " + password.getText().toString();
+        input += "Password: ";// + password.getText().toString();
         Toast.makeText(this, input, Toast.LENGTH_SHORT).show();
 
     }
--- app/src/main/java/com/fyp/yes2live/homepage.java
+++ app/src/main/java/com/fyp/yes2live/homepage.java
@@ -1,34 +1,86 @@
 package com.fyp.yes2live;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
+import com.fyp.yes2live.auth.login;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.ArrayList;
 
 public class homepage extends AppCompatActivity {
-Button button1;
-Button bmi_button;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+    private BottomNavigationView bottomNavigationView;
+    private ImageSlider imageSlider;
+    private RelativeLayout bmiModule;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
-        button1=(Button) findViewById(R.id.dibetic);
-        bmi_button=(Button)findViewById(R.id.bmi);
-       button1.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               Intent intent = new Intent(homepage.this, married.class);
-               startActivity(intent);
-           }
-       });
-        bmi_button.setOnClickListener(new View.OnClickListener() {
+
+        drawerLayout = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigation_view);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        bottomNavigationView=findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setBackground(null);
+        imageSlider = findViewById(R.id.imageSlider);
+
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(homepage.this, bmi_calculator.class);
-                startActivity(intent);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                drawerLayout.closeDrawer(GravityCompat.START);
+                return true;
             }
+        });
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return true;
+            }
+        });
+
+
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel(R.drawable.dietplanslider1, ScaleTypes.CENTER_CROP));
+        slideModels.add(new SlideModel(R.drawable.dietplanslider1,ScaleTypes.CENTER_CROP));
+
+        imageSlider.setImageList(slideModels, ScaleTypes.CENTER_CROP);
+
+
+        bmiModule = (RelativeLayout) findViewById(R.id.bmi);
+
+        bmiModule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(homepage.this, bmi_calculator.class);
+                startActivity(intent1);
+            };
         });
     }
 }
